6aeb23533afb46795d9eacfd36fd8ef0b601ad1f
JungJaeW <nu052308@naver.com>
2023-02-07 00:20:07 +0900

    firstCommit

---
+++ src/components/FilterBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div @click="fire" :class="filter + ' filter-item'" :style="`background-image:url(${image})`">
+    <slot></slot>
+    </div> 
+</template>
+
+<script>
+export default {
+  name : 'filterbox',
+  methods : {
+    fire(){
+      this.emitter.emit('clickBox',this.filter);
+    }
+  },
+  props : {
+    image : String,
+    filter : String,
+  }
+}
+</script>
+
+<style>
+  .filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color : black;
+  background-size: cover;
+  background-position : center;
+}
+</style>
